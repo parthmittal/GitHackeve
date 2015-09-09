f3615035fffab2945cd8a6dfcefa26c0e8b4993b
Jai Luthra <me@jailuthra.in>
2015-09-09 18:55:06 +0530

    prime

--- project1.py
+++ project1.py
@@ -4,8 +4,11 @@ def fib(number_for_fibonacci):
 
 
 def is_prime(number_to_check):
-    # Add code here
-    return #boolean value
+    a = True
+    for i in range(int(number_to_check ** (0.5)) + 1):
+        if number_to_check % i == 0:
+            return False
+    return True
 
 
 def reverse_string(string_to_be_reversed):
@@ -19,7 +22,7 @@ print(fib(a))
 
 
 #Take input for is_prime in variable b
-
+b = int(input())
 print(is_prime(b))
 
 
